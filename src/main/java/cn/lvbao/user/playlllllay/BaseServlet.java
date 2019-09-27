5f4123a41bc05da000ccb692974c89b602e77317
四宝 <lms19990808@163.com>
2019-09-27 21:14:10 +0800

    add user model

--- src/main/java/cn/lvbao/user/playlllllay/BaseServlet.java
+++ src/main/java/cn/lvbao/user/playlllllay/BaseServlet.java
@@ -1,7 +1,6 @@
 package cn.lvbao.user.playlllllay;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- src/test/ms/Test.java
+++ src/test/ms/Test.java
@@ -5,4 +5,8 @@ package ms;
  * #create 2019-09-26-12:37
  */
 public class Test {
+    @Test
+    public void test1(){
+        System.out.println(123);
+    }
 }
